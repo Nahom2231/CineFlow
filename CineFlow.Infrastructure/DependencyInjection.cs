8689c1e7786feb402a13aa95f8c38f0aa7aea541
Nahom2231 <nahom0462@gmail.com>
2026-07-24 04:32:37 +0300

    feat(infrastructure): implement local file storage service

--- CineFlow.Infrastructure/DependencyInjection.cs
+++ CineFlow.Infrastructure/DependencyInjection.cs
@@ -1,10 +1,9 @@
 using CineFlow.Application.Common.Interfaces;
 using CineFlow.Infrastructure.Persistence;
-using CineFlow.Infrastructure.Services;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-
+using CineFlow.Infrastructure.Services;
 namespace CineFlow.Infrastructure;
 
 public static class DependencyInjection
@@ -16,7 +15,7 @@ public static class DependencyInjection
         services.AddDbContext<CineFlowDbContext>(options =>
         options.UseNpgsql(connectionString));
         services.AddScoped<ICineFlowDbContext>(provider =>provider.GetRequiredService<CineFlowDbContext>());
-        services.AddScoped<IFileStorageService, LocalFileStorageService>();
+         services.AddScoped<IFileStorageService, LocalFileStorageService>();
         return services;
     }
 }
