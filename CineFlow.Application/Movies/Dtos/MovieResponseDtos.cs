ce1ab60da73557c70d1e1efb5ac439a9a0e9d76c
Nahom2231 <nahom0462@gmail.com>
2026-08-07 14:27:07 +0300

    fix(Dtos): changing the filename MovieDtos.cs into MovieResponseDtos.cs in CineFlow.Application.

---
+++ CineFlow.Application/Movies/Dtos/MovieResponseDtos.cs
@@ -0,0 +1,25 @@
+namespace  CineFlow.Application.Movies.Dtos;
+
+public class MovieResponseDto
+{
+    public Guid Id {get; set;}
+    public string TitleEnglish {get; set;}=string.Empty;
+
+    public string TitleAmharic {get; set;} = string.Empty;
+    public string DescriptionEnglish {get; set;}=string.Empty;
+
+    public string DescriptionAmharic {get; set;} = string.Empty;
+
+    public int DurationMinutes {get; set;}
+
+    public string Genre {get; set;}=string.Empty;
+    public string AudioLanguage {get;set;}=string.Empty;
+
+    public string FeaturedImageUrl {get; set;}=string.Empty;
+
+    public List<string> GalleryImageUrls {get; set;}=new();
+
+    public string DirectorName {get; set;} = string.Empty;
+
+    public List <string> StarName {get; set;}= new();
+}
