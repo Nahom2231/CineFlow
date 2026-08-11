8c6ed13f9eec0fdb0971bc74a3d9285d39b82f72
Nahom2231 <nahom0462@gmail.com>
2026-08-12 00:29:21 +0300

    feat(cors): Implementing CORS policy with origins by allowing angular on the backend.

--- CineFlow.Api/Program.cs
+++ CineFlow.Api/Program.cs
@@ -9,6 +9,16 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddOpenApi();
 builder.Services.AddApplicationServices();
 builder.Services.AddInfrastructureService(builder.Configuration);
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAngular", policy =>
+    {
+        policy.WithOrigins("http://localhost:4200")
+        .AllowAnyHeader()
+        .AllowAnyMethod();
+
+    });
+});
 builder.Services.AddControllers();
 var app = builder.Build();
 
@@ -18,7 +28,8 @@ if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
-
+app.UseRouting();
+app.UseCors("AllowAngular");
 app.UseHttpsRedirection();
 app.UseAuthentication();
 app.UseAuthorization();
