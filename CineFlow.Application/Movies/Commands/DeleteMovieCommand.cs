0ec366636426ee87056dd5e06b83cb9351b5ce96
Nahom2231 <nahom0462@gmail.com>
2026-08-30 22:09:51 +0300

    refactor(movies): update DeleteMovieCommand

--- CineFlow.Application/Movies/Commands/DeleteMovieCommand.cs
+++ CineFlow.Application/Movies/Commands/DeleteMovieCommand.cs
@@ -23,6 +23,29 @@ public class DeleteMovieCommandHandler : IRequestHandler<DeleteMovieCommand, boo
 
         if (movie == null) return false;
 
+        if (movie.Schedules.Any())
+        {
+            var scheduleIds = movie.Schedules.Select(s => s.Id).ToList();
+
+            var reservations = await _context.SeatReservations
+                .Where(r => scheduleIds.Contains(r.ScheduleId))
+                .ToListAsync(cancellationToken);
+            if (reservations.Any())
+            {
+                _context.SeatReservations.RemoveRange(reservations);
+            }
+
+            var tickets = await _context.Tickets
+                .Where(t => scheduleIds.Contains(t.ScheduleId))
+                .ToListAsync(cancellationToken);
+            if (tickets.Any())
+            {
+                _context.Tickets.RemoveRange(tickets);
+            }
+
+            _context.Schedules.RemoveRange(movie.Schedules);
+        }
+
         _context.Movies.Remove(movie);
         await _context.SaveChangesAsync(cancellationToken);
         return true;
